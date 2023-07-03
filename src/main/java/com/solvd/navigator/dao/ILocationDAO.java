feaf098f9a05407204a66a53f3d71be16d0a7cc3
vvelediaz <vvelediaz@gmail.com>
2023-07-03 07:46:33 -0700

    Added models to daos

--- src/main/java/com/solvd/navigator/dao/ILocationDAO.java
+++ src/main/java/com/solvd/navigator/dao/ILocationDAO.java
@@ -1,5 +1,7 @@
 package com.solvd.navigator.dao;
 
+import com.solvd.navigator.model.Location;
+
 public interface ILocationDAO extends IDAO<Location>{
 
 }
--- src/main/java/com/solvd/navigator/dao/IRouteDAO.java
+++ src/main/java/com/solvd/navigator/dao/IRouteDAO.java
@@ -1,6 +1,7 @@
 package com.solvd.navigator.dao;
 
+import com.solvd.navigator.model.Route;
+
 public interface IRouteDAO extends IDAO<Route>{
     Route getRouteByLocationsId(long locationAId, long locationBId);
-
 }
